da4df1599277456c93f5f9ba562128c87d9360a1
Hawa Kallon <hawakallon58@gmail.com>
2024-10-07 10:07:36 +0000

    edits

--- pipeline/src/lib/TeamForm.svelte
+++ pipeline/src/lib/TeamForm.svelte
@@ -5,10 +5,10 @@
         <label for="email" class="mb-5">Email</label>
         <input type="email" id="email" class="flex gap-2 w-full border border-lime-800 border-solid min-h-[80px] rounded-[52px] max-md:max-w-full px-8" required />
       </div>
-      <div class="flex flex-col mb-10">
+      <!-- <div class="flex flex-col mb-10">
         <label for="title" class="mb-5">Title</label>
         <input type="text" id="title" class="flex gap-2 w-full border border-lime-800 border-solid min-h-[80px] rounded-[52px] max-md:max-w-full px-8" required />
-      </div>
+      </div> -->
       <div class="flex flex-wrap gap-10 justify-between items-center mt-10 w-full text-xl font-medium text-gray-200 max-md:max-w-full">
         <button type="submit" class="gap-1 self-stretch px-12 py-8 my-auto bg-cyan-950 min-w-[240px] rounded-[82px] w-[696px] max-md:px-5 max-md:max-w-full">
           Send Invitation
