d7df860d4b125113f24a38af2fb705fc857abc9d
saidubundukamara <bundukamarasaidu@gmail.com>
2024-10-30 15:37:38 +0000

    load categories and sort projects

---
+++ src/routes/api/categories/+server.js
@@ -0,0 +1,16 @@
+import { supabase } from '$lib/server/supabase.js';
+import { json } from '@sveltejs/kit';
+
+export async function GET({ request }) {
+    try {
+
+        const { data, error } = await supabase
+          .from('categories')
+          .select('*');
+
+        return json({ categories: data }, { status: 200 });
+        
+    } catch (error) {
+        return json({ error: error.message }, { status: 500 });
+    }
+}
