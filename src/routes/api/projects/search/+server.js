a1420f6d72ab4e1a94a2e4f0c5214f8cf7640782
saidubundukamara <bundukamarasaidu@gmail.com>
2024-11-07 12:45:47 +0000

    api search projects code format

--- src/routes/api/projects/search/+server.js
+++ src/routes/api/projects/search/+server.js
@@ -1,27 +1,27 @@
-import { supabase } from '$lib/server/supabase.js';
-import { json } from '@sveltejs/kit';
+import { supabase } from "$lib/server/supabase.js";
+import { json } from "@sveltejs/kit";
 
 export async function GET({ url }) {
-    const page = parseInt(url.searchParams.get('page') || '1', 10);
-    const limit = parseInt(url.searchParams.get('limit') || '6', 10);
-    const term = url.searchParams.get('term') || '';
-    const start = (page - 1) * limit;
-    const end = start + limit - 1;
+  const page = parseInt(url.searchParams.get("page") || "1", 10);
+  const limit = parseInt(url.searchParams.get("limit") || "6", 10);
+  const term = url.searchParams.get("term") || "";
+  const start = (page - 1) * limit;
+  const end = start + limit - 1;
 
-    console.log('term:', term);
-  
-    try {
-      const { data, error } = await supabase
-        .from('projects')
-        .select('*')
-        .ilike('title', `%${term}%`)
-        //.ilike('description', `%${term}%`)
-        .range(start, end); 
-  
-      if (error) throw error;
-  
-      return json({ projects: data }, { status: 200 });
-    } catch (error) {
-      return json({ error: error.message }, { status: 500 });
-    }
+  console.log("term:", term);
+
+  try {
+    const { data, error } = await supabase
+      .from("projects")
+      .select("*")
+      .ilike("title", `%${term}%`)
+      //.ilike('description', `%${term}%`)
+      .range(start, end);
+
+    if (error) throw error;
+
+    return json({ projects: data }, { status: 200 });
+  } catch (error) {
+    return json({ error: error.message }, { status: 500 });
   }
+}
