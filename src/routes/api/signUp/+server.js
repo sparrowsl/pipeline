b95d7fa3471a46641363429fc0c8232971525df4
Colin Ogoo <ogoo.colin@gmail.com>
2024-11-07 17:05:47 +0000

    feat(auth): use supabase admin
    create user via admin

--- src/routes/api/signUp/+server.js
+++ src/routes/api/signUp/+server.js
@@ -1,36 +1,32 @@
-import { supabase } from '$lib/server/supabase.js';
+import { supabase, adminAuthClient } from '$lib/server/supabase.js';
 import { json, redirect } from '@sveltejs/kit';
 
 export async function POST({ request }) {
   try {
     const { email, password, name } = await request.json();
 
-    const { data: signUpData, error: signUpError } = await supabase.auth.signUp({ 
-        email, 
-        password,
-        options: {
-            emailRedirectTo: 'http://localhost:5173/signIn'
-        }
+    const { data: signUpData, error: signUpError } = await adminAuthClient.createUser({
+      email,
+      password,
+      email_confirm: true,
+      user_metadata: {
+        name,
+      },
     });
 
     if (signUpError) {
-        return json({ error: signUpError.message }, { status: 400 });
-      }
-  
-      
-      const { data: userData, error: profileError } = await supabase
-        .from('profiles')
-        .insert([
-          { user_id: signUpData.user.id, name: name } 
-        ]);
-  
-      if (profileError) {
-        return json({ error: profileError.message }, { status: 500 });
-      } 
+      return json({ error: signUpError.message }, { status: 400 });
+    }
 
-       
+    const { data: userData, error: profileError } = await supabase
+      .from('profiles')
+      .insert([{ user_id: signUpData.user.id, name: name }]);
+
+    if (profileError) {
+      return json({ error: profileError.message }, { status: 500 });
+    }
+
     throw redirect(303, '/waiting-confirmation');
-
   } catch (err) {
     return new Response(JSON.stringify({ error: 'Failed to sign up' }), { status: 500 });
   }
