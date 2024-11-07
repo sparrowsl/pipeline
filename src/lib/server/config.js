63c717ad1d787cb79bb65ebe6fe6904b99b2ea0d
Colin Ogoo <ogoo.colin@gmail.com>
2024-11-07 17:03:17 +0000

    fix(env): standardise env variables

--- src/lib/server/config.js
+++ src/lib/server/config.js
@@ -3,3 +3,6 @@ import { env } from '$env/dynamic/private';
 
 export const OPENAI_API_KEY = env.PRIVATE_OPENAI_API_KEY;
 export const GITHUB_TOKEN = env.PRIVATE_GITHUB_TOKEN;
+export const SUPABASE_SERVICE_KEY = env.PRIVATE_SUPABASE_SERVICE_KEY;
+export const supabaseUrl = env.VITE_SUPABASE_URL;
+export const supabaseAnonKey = env.VITE_SUPERBASE_ANON_KEY;
