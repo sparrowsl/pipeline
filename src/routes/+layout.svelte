6c551160459958ba5db33d653665cc532c939ce4
Colin Ogoo <ogoo.colin@gmail.com>
2024-11-12 15:09:08 +0000

    refactor(sign in): rename signIn -> sign-in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,20 @@
 <script>
-  import "./app.css";
-  import Nav from "$lib/Nav.svelte";
-  import Footer from "$lib/Footer.svelte";
-  import { page } from "$app/stores"; // access the current page route
+  import './app.css';
+  import Nav from '$lib/Nav.svelte';
+  import Footer from '$lib/Footer.svelte';
+  import { page } from '$app/stores'; // access the current page route
 
   export let data;
 </script>
 
 <div class="w-full min-h-screen bg-white">
-  {#if $page.url.pathname !== "/signIn"}
+  {#if $page.url.pathname !== '/sign-in'}
     <Nav {data} />
   {/if}
 
   <slot />
 
-  {#if $page.url.pathname !== "/signIn"}
+  {#if $page.url.pathname !== '/sign-in'}
     <Footer />
   {/if}
 </div>
