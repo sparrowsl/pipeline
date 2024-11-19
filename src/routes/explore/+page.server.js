3432996fed216e80a390572a8a16f474cc793dd9
sparrowsl <benthorpe.dev@gmail.com>
2024-11-19 18:08:47 +0000

    run formatter

--- src/routes/explore/+page.server.js
+++ src/routes/explore/+page.server.js
@@ -1,19 +1,19 @@
-import { error } from "@sveltejs/kit";
+import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ url }) {
-	const currentPage = url.searchParams.get("page") || 1;
-	const itemsPerPage = url.searchParams.get("limit") || 6;
+  const currentPage = url.searchParams.get('page') || 1;
+  const itemsPerPage = url.searchParams.get('limit') || 6;
 
-	const [allProjects, topProjects] = await Promise.allSettled([
-		fetchAllProjects(currentPage, itemsPerPage),
-		fetchTopProjects(),
-	]);
+  const [allProjects, topProjects] = await Promise.allSettled([
+    fetchAllProjects(currentPage, itemsPerPage),
+    fetchTopProjects(),
+  ]);
 
-	return {
-		allProjects,
-		topProjects,
-	};
+  return {
+    allProjects,
+    topProjects,
+  };
 }
 
 /**
@@ -22,29 +22,27 @@ export async function load({ url }) {
  * @returns {Promise<any>}
  */
 async function fetchAllProjects(page = 1, limit = 6) {
-	try {
-		const response = await fetch(
-			`/api/projects/allprojects?page=${page}&limit=${limit}`,
-		);
-		if (!response.ok) {
-			return [];
-		}
+  try {
+    const response = await fetch(`/api/projects/allprojects?page=${page}&limit=${limit}`);
+    if (!response.ok) {
+      return [];
+    }
 
-		return await response.json();
-	} catch (e) {
-		error(404);
-	}
+    return await response.json();
+  } catch (e) {
+    error(404);
+  }
 }
 
 async function fetchTopProjects() {
-	try {
-		const response = await fetch("/api/projects/allprojects?limit=3");
-		if (!response.ok) {
-			return [];
-		}
+  try {
+    const response = await fetch('/api/projects/allprojects?limit=3');
+    if (!response.ok) {
+      return [];
+    }
 
-		return await response.json();
-	} catch (e) {
-		error(404);
-	}
+    return await response.json();
+  } catch (e) {
+    error(404);
+  }
 }
