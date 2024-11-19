e0c02b5e05e87037dab7862d2566da0448f24985
sparrowsl <benthorpe.dev@gmail.com>
2024-11-19 18:05:38 +0000

    refactor: move fetch functions outside of load function

--- src/routes/explore/+page.server.js
+++ src/routes/explore/+page.server.js
@@ -1,39 +1,12 @@
+import { error } from "@sveltejs/kit";
+
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ fetch, url }) {
-	const itemsPerPage = url.searchParams.get("limit") || 6;
+export async function load({ url }) {
 	const currentPage = url.searchParams.get("page") || 1;
+	const itemsPerPage = url.searchParams.get("limit") || 6;
 
-	async function fetchAllProjects() {
-		try {
-			const response = await fetch(
-				`/api/projects/allprojects?page=${currentPage}&limit=${itemsPerPage}`,
-			);
-			if (!response.ok) {
-				return [];
-			}
-
-			return await response.json();
-		} catch (error) {
-			// TODO: do something with the error
-		}
-	}
-
-	async function fetchTopProjects() {
-		try {
-			const response = await fetch("/api/projects/allprojects?limit=3");
-			if (!response.ok) {
-				return [];
-			}
-
-			return await response.json();
-		} catch (error) {
-			// TODO: do something with the error
-		}
-	}
-
-	// TODO: could be changed to `Promise.allSettled` in case one of the functions failed
 	const [allProjects, topProjects] = await Promise.allSettled([
-		fetchAllProjects(),
+		fetchAllProjects(currentPage, itemsPerPage),
 		fetchTopProjects(),
 	]);
 
@@ -41,4 +14,37 @@ export async function load({ fetch, url }) {
 		allProjects,
 		topProjects,
 	};
+}
+
+/**
+ * @param {number} page
+ * @param {number} limit
+ * @returns {Promise<any>}
+ */
+async function fetchAllProjects(page = 1, limit = 6) {
+	try {
+		const response = await fetch(
+			`/api/projects/allprojects?page=${page}&limit=${limit}`,
+		);
+		if (!response.ok) {
+			return [];
+		}
+
+		return await response.json();
+	} catch (e) {
+		error(404);
+	}
+}
+
+async function fetchTopProjects() {
+	try {
+		const response = await fetch("/api/projects/allprojects?limit=3");
+		if (!response.ok) {
+			return [];
+		}
+
+		return await response.json();
+	} catch (e) {
+		error(404);
+	}
 }
