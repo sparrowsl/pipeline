49e04a4f48528e1ad75e2e862501747cbe2e3aff
Colin Ogoo <ogoo.colin@gmail.com>
2024-11-21 12:35:15 +0000

    refactor(explore server): simplify code and reuse handler

--- src/routes/explore/+page.server.js
+++ src/routes/explore/+page.server.js
@@ -1,48 +1,19 @@
 import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ url }) {
+export async function load({ url, fetch }) {
   const currentPage = url.searchParams.get('page') || 1;
   const itemsPerPage = url.searchParams.get('limit') || 6;
 
-  const [allProjects, topProjects] = await Promise.allSettled([
-    fetchAllProjects(currentPage, itemsPerPage),
-    fetchTopProjects(),
+  const [{ projects: allProjects }, { projects: topProjects }] = await Promise.all([
+    fetch(`/api/projects?page=${currentPage}&limit=${itemsPerPage}`).then(parseJsonResponse),
+    fetch(`/api/projects?limit=3`).then(parseJsonResponse),
   ]);
 
-  return {
-    allProjects,
-    topProjects,
-  };
+  return { allProjects, topProjects };
 }
 
-/**
- * @param {number} page
- * @param {number} limit
- * @returns {Promise<any>}
- */
-async function fetchAllProjects(page = 1, limit = 6) {
-  try {
-    const response = await fetch(`/api/projects/allprojects?page=${page}&limit=${limit}`);
-    if (!response.ok) {
-      return [];
-    }
-
-    return await response.json();
-  } catch (e) {
-    error(404);
-  }
-}
-
-async function fetchTopProjects() {
-  try {
-    const response = await fetch('/api/projects/allprojects?limit=3');
-    if (!response.ok) {
-      return [];
-    }
-
-    return await response.json();
-  } catch (e) {
-    error(404);
-  }
-}
+const parseJsonResponse = (res) => {
+  if (!res.ok) throw error(404);
+  return res.json();
+};
