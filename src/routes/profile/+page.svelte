9f55ac14dd05e0c0b774e3563dbe5385f0ec92de
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-11-23 01:27:54 +0000

    Profile page: project session distortion
    after navigating to the create project page, when navigating back to the profile page, the project section gets distorted.

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-  import ProfileInfo from "../../lib/ProfileInfo.svelte";
-  import ProjectSection from "../../lib/ProjectSection.svelte";
-  import { onMount } from "svelte";
+  import ProfileInfo from '../../lib/ProfileInfo.svelte';
+  import ProjectSection from '../../lib/ProjectSection.svelte';
+  import { onMount } from 'svelte';
 
   let userProjects = [];
   let loading = true;
@@ -9,10 +9,10 @@
 
   async function fetchMyProjects() {
     try {
-      const response = await fetch("/api/projects/myProjects", {
-        method: "GET",
+      const response = await fetch('/api/projects/myProjects', {
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       });
 
@@ -37,20 +37,24 @@
   });
 </script>
 
-<div class="flex flex-col md:flex-row w-[90%] mx-auto space-y-4 md:space-y-0 md:space-x-4 mt-[-25px]">
+<!-- new code -->
+<!-- container below to center the inner container -->
+<div class="flex w-full items-start justify-center">
+  <div class="w-[90%] mx-auto grid justify-center md:flex md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+    
+    <div class="px-5 md:px-0 md:w-1/3 bg-yellow-50 md:sticky h-max">
+      <ProfileInfo />
+    </div>
 
-  <div class="flex-none w-full md:w-1/3 md:sticky md:top-4 h-max">
-    <ProfileInfo />
-  </div>
-
-  <div class="flex-1">
-    {#if loading}
-      <p>Loading projects...</p>
-    {:else if error}
-      <p>Error: {error}</p>
-    {:else}
-      <ProjectSection projects={userProjects} />
-    {/if}
+ 
+    <div class="md:w-2/3">
+      {#if loading}
+        <p>Loading projects...</p>
+      {:else if error}
+        <p>Error: {error}</p>
+      {:else}
+        <ProjectSection projects={userProjects} />
+      {/if}
+    </div>
   </div>
 </div>
-
