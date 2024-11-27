a8381454088cb1c9d627b86d5c0c055d519a5ac4
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-11-27 17:32:30 +0000

    reduced 404 image width

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,10 +5,10 @@
 <!-- if the status code is "404" -->
 {#if $page.status === 404}
 <div class="flex flex-col items-center justify-center mt-16">
-  <img loading="lazy" src="/404.png" alt="404" class="object-cover w-full md:w-[90%] md:h-[500px]"/>
+  <img loading="lazy" src="/404.png" alt="404" class="object-cover w-full md:w-[70%] md:h-[500px]"/>
   <a
     href="/explore"
-    class="bg-[#d1ea9a] hover:bg-[#e1faac] text-base font-semibold py-2 px-4 rounded-lg -mt-5 w-[100px] text-center">
+    class="bg-[#d1ea9a] hover:bg-[#e1faac] text-base font-semibold py-2 px-4 rounded-lg w-[100px] text-center">
     Home
   </a>
 </div>
