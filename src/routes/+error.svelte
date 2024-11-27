31a3c2449feb101ed1d0d434e71501251ee32459
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-11-27 14:03:20 +0000

    Responsive error page
    Responsiveness of the error page on all device

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,6 +5,6 @@
 <!-- if the status code is "404" -->
 {#if $page.status === 404}
 <div class="flex flex-col items-center justify-center mt-16">
- <img src="/404.png" alt="404" class="object-cover h-[430px]" />
+ <img src="/404.png" alt="404" class="object-cover w-full md:w-auto md:h-[430px]" />
 </div>
 {/if}
