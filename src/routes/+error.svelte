615130715f448f9bfb3433da6062c4a0e187a3b9
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-11-27 14:19:57 +0000

    updated link and image
    Link to home added, and loading="Lazy" added .

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,6 +5,11 @@
 <!-- if the status code is "404" -->
 {#if $page.status === 404}
 <div class="flex flex-col items-center justify-center mt-16">
- <img src="/404.png" alt="404" class="object-cover w-full md:w-auto md:h-[430px]" />
+ <img loading="lazy" src="/404.png" alt="404" class="object-cover w-full md:w-auto md:h-[430px]"/>
+ <a
+    href="/explore"
+    class="bg-[#d1ea9a] hover:bg-[#e1faac] text-base font-semibold py-2 px-4 rounded -mt-5">
+    Home
+  </a>
 </div>
 {/if}
