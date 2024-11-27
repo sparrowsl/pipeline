3923068b0e72ca45bdd9f52b64ede3a88be2ad2c
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-11-27 12:41:11 +0000

    404 error page update
    updated design and fixing the redundancy

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,26 +2,9 @@
   import { page } from '$app/stores';
 </script>
 
-<!-- if the status code is "404" and the error message is "not found" -->
-{#if $page.status === 404 && $page.error.message === "Not Found"}
+<!-- if the status code is "404" -->
+{#if $page.status === 404}
 <div class="flex flex-col items-center justify-center mt-16">
-  <h1 class="text-9xl font-bold text-gray-800">{$page.status}</h1>
-  <p class="text-2xl font-medium text-gray-600 mt-4">Oops! Page not found.</p>
-  <a
-    href="/explore"
-    class="bg-[#d1ea9a] hover:bg-[#e1faac] text-base font-bold py-2 px-4 rounded mt-4">
-    Go Home
-  </a>
+ <img src="/404.png" alt="404" class="object-cover h-[430px]" />
 </div>
 {/if}
-
-<!-- if the status code is "404" and the error message is "Error: 404" -->
-{#if $page.status === 404 && $page.error.message === "Error: 404"}
-<div class="flex flex-col items-center justify-center mt-16">
-  <h1 class="text-9xl font-bold text-gray-800">{$page.status}</h1>
-  <p class="text-2xl font-medium text-gray-600 mt-4">Oops! Something is not loading.</p>
-</div>
-{/if}
-
-<!-- other error codes implementation below here -->
-
