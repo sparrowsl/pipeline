422de8057e3ec85f3f8190e102bfafe5dcf6c063
Hawa Kallon <hawakallon58@gmail.com>
2024-12-04 15:19:46 +0000

    feat(fix nav: solve issue with navbar #221

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -209,82 +209,127 @@
 
   <!-- Mobile Menu -->
   {#if isMobileMenuOpen}
-    <div class="absolute top-[84px] left-0 right-0 bg-[#0b383c] md:hidden">
-      <div class="flex flex-col p-4 space-y-4">
-        <!-- Mobile Search -->
-        <SearchDropdown />
-
-        <a href="/" class="text-white text-base font-semibold font-['Inter']"> Tasks </a>
-
-        <!-- Mobile Resources Dropdown -->
-        <div class="relative resources-dropdown">
+  <div class="absolute top-[84px] left-0 right-0 bg-[#0b383c] md:hidden">
+    <div class="flex flex-col p-4 space-y-4">
+      <!-- Mobile Search -->
+      <div class="w-full max-w-[480px] justify-center items-center mb-2">
+        <div
+          class="w-full pl-4 pr-3 bg-[#115d5b] rounded-[48.77px] flex justify-between items-center align-center py-2"
+        >
           <button
-            on:click={toggleResources}
-            class="flex items-center justify-between w-full px-4 py-4 border-b focus:outline-none border-cyan-800"
+            type="button"
+            class="flex justify-between w-full mt-2"
+            on:click={() => ($searchBarOpen = !$searchBarOpen)}
           >
-            <span class="text-white text-base font-semibold font-['Inter'] leading-none ml-[-18px]"
-              >Resources</span
-            >
-            <svg
-              width="13"
-              height="12"
-              viewBox="0 0 13 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-[17px] transform transition-transform duration-200 {isResourcesOpen
-                ? 'rotate-180'
-                : ''}"
-            >
-              <g id="CaretDown" clip-path="url(#clip0_1224_8929)">
-                <path
-                  id="Vector"
-                  d="M10.0837 4.5L6.33374 8.25L2.58374 4.5"
-                  stroke="white"
-                  stroke-width="1.67"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_1224_8929">
-                  <rect width="12" height="12" fill="white" transform="translate(0.334106)" />
-                </clipPath>
-              </defs>
-            </svg>
+            <div class="text-white text-sm font-['Inter'] ">Search for a project...</div>
+            <span aria-label="Search" class="relative h-7 w-7">
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="MagnifyingGlass">
+                  <path
+                    id="Vector"
+                    d="M8.75 15.001C12.2018 15.001 15 12.2028 15 8.75098C15 5.2992 12.2018 2.50098 8.75 2.50098C5.29822 2.50098 2.5 5.2992 2.5 8.75098C2.5 12.2028 5.29822 15.001 8.75 15.001Z"
+                    fill="#0D909C"
+                    fill-opacity="0.3"
+                    stroke="#A0A0A0"
+                    stroke-width="1.00699"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    id="Vector_2"
+                    d="M13.1697 13.1699L17.5001 17.5004"
+                    stroke="#A0A0A0"
+                    stroke-width="1.00699"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </g>
+              </svg>
+            </span>
           </button>
-
-          {#if isResourcesOpen}
-            <div
-              class="absolute right-0 z-[9999] top-6 w-[25vh] mt-2 rounded-md shadow-lg bg-cyan-900"
-            >
-              <a
-                href="/resources/pipeline"
-                class="block w-full px-4 py-3 text-left text-[#d1ea9a] hover:bg-cyan-800"
-                >About Pipeline</a
-              >
-              <a
-                href="/resources/digital-public-good"
-                class="block w-full px-4 py-3 text-left text-[#d1ea9a] hover:bg-cyan-800"
-                >About DPGs</a
-              >
-            </div>
-          {/if}
-        </div>
-
-        <!-- Mobile Authentication -->
-        <div>
-          {#if data.isAuthenticated}
-            <UserProfile {data} />
-          {:else}
-            <a
-              href="/sign-in"
-              class="block w-full text-center px-4 py-4 bg-[#d1ea9a] rounded-3xl text-base font-semibold"
-            >
-              Sign up / Log in
-            </a>
-          {/if}
         </div>
       </div>
+      
+
+      <!-- Other Menu Items -->
+      <a href="/" class="text-white text-base font-semibold font-['Inter']"> Tasks </a>
+
+      <!-- Mobile Resources Dropdown -->
+      <div class="relative resources-dropdown">
+        <button
+          on:click={toggleResources}
+          class="flex items-center justify-between w-full px-4 py-4 border-b focus:outline-none border-cyan-800"
+        >
+          <span class="text-white text-base font-semibold font-['Inter'] leading-none ml-[-18px]">
+            Resources
+          </span>
+          <svg
+            width="13"
+            height="12"
+            viewBox="0 0 13 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-[17px] transform transition-transform duration-200 {isResourcesOpen
+              ? 'rotate-180'
+              : ''}"
+          >
+            <g id="CaretDown" clip-path="url(#clip0_1224_8929)">
+              <path
+                id="Vector"
+                d="M10.0837 4.5L6.33374 8.25L2.58374 4.5"
+                stroke="white"
+                stroke-width="1.67"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1224_8929">
+                <rect width="12" height="12" fill="white" transform="translate(0.334106)" />
+              </clipPath>
+            </defs>
+          </svg>
+        </button>
+
+        {#if isResourcesOpen}
+          <div
+            class="absolute right-0 z-[9999] top-6 w-[25vh] mt-2 rounded-md shadow-lg bg-cyan-900"
+          >
+            <a
+              href="/resources/pipeline"
+              class="block w-full px-4 py-3 text-left text-[#d1ea9a] hover:bg-cyan-800"
+              >About Pipeline</a
+            >
+            <a
+              href="/resources/digital-public-good"
+              class="block w-full px-4 py-3 text-left text-[#d1ea9a] hover:bg-cyan-800"
+              >About DPGs</a
+            >
+          </div>
+        {/if}
+      </div>
+
+      <!-- Mobile Authentication -->
+      <div>
+        {#if data.isAuthenticated}
+          <UserProfile {data} />
+        {:else}
+          <a
+            href="/sign-in"
+            class="block w-full text-center px-4 py-4 bg-[#d1ea9a] rounded-3xl text-base font-semibold"
+          >
+            Sign up / Log in
+          </a>
+        {/if}
+      </div>
     </div>
-  {/if}
+  </div>
+{/if}
+
 </header>
