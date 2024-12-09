f52fa29c1f6f36fa5675b5b9c4e500c97dbe226e
Hawa Kallon <97176661+HawaKallon@users.noreply.github.com>
2024-12-09 17:09:13 +0000

    responsiveness #87 (#236)

--- src/lib/UpdateDetail.svelte
+++ src/lib/UpdateDetail.svelte
@@ -159,7 +159,7 @@
     </div>
   </div>
 
-  <div class="self-stretch pt-[42px] flex-col justify-start items-start gap-[18px] flex">
+  <!-- <div class="self-stretch pt-[42px] flex-col justify-start items-start gap-[18px] flex">
     <div class="flex flex-col items-start self-stretch justify-start h-5">
       <div class="self-stretch text-[#282828] text-base font-bold font-['Inter'] leading-tight">
         Comments ({comments.length})
@@ -243,6 +243,95 @@
       </div>
     {:else}
       <p>No comments</p>
+    {/if}
+  </div> -->
+
+  <div class="self-stretch pt-[42px] flex-col justify-start items-start gap-[18px] flex">
+    <div class="flex flex-col items-start self-stretch justify-start h-5">
+      <div class="self-stretch text-[#282828] text-base font-bold font-['Inter'] leading-tight">
+        Comments ({comments.length})
+      </div>
+    </div>
+    
+    <div class="flex flex-col w-full gap-4 pt-4 pb-8 md:flex-row md:justify-between md:items-center">
+      {#if data.isAuthenticated}
+        <div class="flex flex-col w-full gap-4 md:flex-row">
+          <input
+            type="text"
+            bind:value={newComment}
+            placeholder="Add a comment..."
+            class="w-full border-2 border-[#dcdedd] rounded-lg px-4 py-2 text-base text-[#0b383c] focus:outline-none focus:border-[#0b383c] transition-colors duration-200 
+                   md:w-3/4"
+          />
+          <button
+            on:click={addUpdateComment}
+            class="md:w-1/4 px-4 py-2 bg-[#0b383c] text-white text-base rounded-lg transition-colors duration-300 focus:outline-none focus:border-[#0b383c]
+                   mt-2 md:mt-0 self-end"
+            disabled={loading}
+          >
+            {loading ? 'Submitting...' : 'Comment'}
+          </button>
+        </div>
+      {:else}
+        <span class="text-sm text-gray-700">
+          <a
+            href="/sign-in"
+            class="font-semibold text-teal-600 transition-colors duration-200 hover:text-teal-800"
+            >Login</a
+          > to comment
+        </span>
+      {/if}
+    </div>
+
+    {#if comments.length > 0}
+      <div
+        class="self-stretch max-h-[385px] overflow-y-auto px-[13px] pt-[15px] pb-[13px] bg-[#fbfbfa] border border-[#e8e8e8] flex-col justify-start items-start gap-3 flex"
+      >
+        {#each comments as comment}
+          <div
+            class="self-stretch p-4 bg-white border border-[#e8e8e8] flex-col justify-start items-start gap-[18px] flex"
+          >
+            <div class="inline-flex items-start self-stretch justify-start">
+              <div class="flex items-start self-stretch justify-start gap-3">
+                <div class="inline-flex flex-col items-start justify-start">
+                  <img
+                    class="w-9 h-9 rounded-full border border-[#dcdedd]"
+                    src={comment.userProfile.image && comment.userProfile.image !== '' ? comment.userProfile.image : defaultImageUrl}
+                    alt=""
+                  />
+                </div>
+                <div class="inline-flex flex-col items-start justify-start flex-grow">
+                  <div class="inline-flex items-center self-stretch justify-start gap-3">
+                    <div
+                      class="text-[#282828] text-sm font-normal font-['Inter'] leading-[21px]"
+                    >
+                      {comment.userProfile.name}
+                    </div>
+                  </div>
+                  <div class="flex flex-col items-start self-stretch justify-start">
+                    <div class="text-[#9b9e9e] text-xs font-normal font-['Inter'] leading-[18px]">
+                      {comment.created_at}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="inline-flex items-start self-stretch justify-center">
+              <div class="flex flex-col items-start self-stretch justify-start w-full">
+                <div class="flex flex-col items-start self-stretch justify-start">
+                  <div
+                    class="self-stretch text-[#282828] text-sm font-normal font-['Inter'] leading-normal"
+                  >
+                    {comment.body}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div>
+    {:else}
+      <p class="italic text-gray-500">No comments yet</p>
     {/if}
   </div>
 </div>
--- src/lib/Updates.svelte
+++ src/lib/Updates.svelte
@@ -8,7 +8,7 @@
   const dispatch = createEventDispatcher();
 
   function showDetail() {
-    dispatch('showDetail', { update });
+    dispatch('showDetail', { update }); 
   }
 
   export let update;
@@ -18,9 +18,9 @@
   const maxLength = 850;
 
   function truncateContent(content) {
-    const strippedContent = content.replace(/<\/?[^>]+(>|$)/g, '');
-    return strippedContent.length > maxLength
-      ? strippedContent.slice(0, maxLength) + '...'
+    const strippedContent = content.replace(/<\/?[^>]+(>|$)/g, ""); 
+    return strippedContent.length > maxLength 
+      ? strippedContent.slice(0, maxLength) + '...' 
       : strippedContent;
   }
 
@@ -33,41 +33,35 @@
   date = dateTimeFormat(update.created_at);
 </script>
 
-<div class="inline-flex flex-col items-start justify-start bg-white p-9">
+<div class="inline-flex flex-col items-start justify-start w-full p-4 bg-white md:p-9">
   <div class="flex flex-col items-start self-stretch justify-start gap-2">
-    <div class=" pt-[1.75px] flex-col justify-start items-start gap-[13.30px] flex">
-      <div class="self-stretch text-[#282828] text-[32px] font-bold font-['Inter'] leading-10">
+    <div class="w-full pt-[1.75px] flex-col justify-start items-start gap-[13.30px] flex">
+      <div class="self-stretch text-[#282828] text-2xl md:text-[32px] font-bold font-['Inter'] leading-tight md:leading-10">
         {update.title}
       </div>
-      <div
-        class="self-stretch h-[62px] pb-5 border-b border-[#dcdedd] flex-col justify-start items-start gap-1 flex"
-      >
-        <div class="inline-flex items-center justify-start gap-3">
+      <div class="self-stretch pb-5 border-b border-[#dcdedd] flex-col justify-start items-start gap-1 flex">
+        <div class="inline-flex items-center justify-start w-full gap-3">
           <img
             loading="lazy"
-            src={update.userProfile.image && update.userProfile.image !== ''
-              ? update.userProfile.image
+            src={update.userProfile.image && update.userProfile.image !== '' 
+              ? update.userProfile.image 
               : defaultImageUrl}
             alt="User Profile"
-            class="w-[42px] h-[42px] relative rounded-[42px] border border-[#dcdedd]"
+            class="w-10 h-10 md:w-[42px] md:h-[42px] rounded-full border border-[#dcdedd]"
           />
 
           <div class="inline-flex flex-col justify-between w-full">
-            <div class="inline-flex items-center self-stretch justify-between gap-2">
-              <div class=" h-6 text-[#282828] text-sm font-normal font-['Inter'] leading-normal">
+            <div class="inline-flex items-center self-stretch gap-2">
+              <div class="text-[#282828] text-sm font-normal font-['Inter'] leading-normal truncate max-w-[200px] md:max-w-none">
                 {update.userProfile.name}
               </div>
-              <div
-                class="px-[5px] py-[0.25px] bg-[#05ce78] rounded-[3px] flex-col justify-center items-start inline-flex"
-              >
+              <div class="px-[5px] py-[0.25px] bg-[#05ce78] rounded-[3px] flex-col justify-center items-start inline-flex">
                 <div class="text-white text-xs font-bold font-['Inter'] leading-[18px]">
                   {update.user_id === update.userProfile.user_id ? 'Creator' : 'Member'}
                 </div>
               </div>
             </div>
-            <div
-              class="self-stretch h-[18px] text-[#282828]/50 text-[13px] font-normal font-['Inter'] leading-[18px]"
-            >
+            <div class="self-stretch text-[#282828]/50 text-[13px] font-normal font-['Inter'] leading-[18px]">
               {date}
             </div>
           </div>
@@ -76,28 +70,23 @@
     </div>
     <div class="flex flex-col items-start self-stretch justify-start gap-1">
       <div class="self-stretch pb-2 flex-col justify-start items-start gap-[30px] flex">
-        <div
-          class="self-stretch text-[#282828] text-base font-normal font-['Inter'] leading-[29px]"
-        >
+        <div class="self-stretch text-[#282828] text-base font-normal font-['Inter'] leading-relaxed">
           {@html truncatedContent}
         </div>
       </div>
     </div>
-    <div class="w-[744.66px] bg-gradient-to-b from-white to-white"></div>
-    <div class="w-[744.66px] justify-between items-center inline-flex">
+    <div class="inline-flex items-center justify-between w-full">
       <div class="flex items-center justify-start gap-6">
         <div class="flex items-center justify-start gap-2">
-          <Icon icon="mdi:chat-outline" class="text-2xl" />
-          <div
-            class="w-[15.25px] h-6 text-[#9b9e9e] text-sm font-normal font-['Inter'] leading-normal"
-          >
+          <Icon icon="mdi:chat-outline" class="text-2xl text-[#8C8C8C]" />
+          <div class="text-[#9b9e9e] text-sm font-normal font-['Inter'] leading-normal">
             16
           </div>
         </div>
       </div>
-      <button
-        on:click={showDetail}
-        class="px-[19px] py-[11.50px] bg-white border border-[#d1d1d1] justify-center items-center gap-1.5 flex"
+      <button 
+        on:click={showDetail} 
+        class="px-4 md:px-[19px] py-2 md:py-[11.50px] bg-white border border-[#d1d1d1] justify-center items-center gap-1.5 flex"
       >
         <div class="text-center text-[#222222] text-sm font-normal">Read more</div>
         <Icon icon="mdi:chevron-right" class="text-2xl" />
