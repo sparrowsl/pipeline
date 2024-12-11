02956533cd0bcebfcbd5f1a3f3fbad5eac85de45
Hawa Kallon <hawakallon58@gmail.com>
2024-12-11 16:28:47 +0000

    fix(Contribute): integrate project details into the project contribute page #91

--- src/lib/Contribute.svelte
+++ src/lib/Contribute.svelte
@@ -1,6 +1,11 @@
 <script>
   import Icon from '@iconify/svelte';
   import Wallet from './Wallet.svelte';
+  import { countries } from 'countries-list';
+
+
+  const countryList = Object.values(countries).sort((a, b) => a.name.localeCompare(b.name));
+
 
   let selectedTab = 'Card';
   let cardNumber = '1264 1234 1234 1234';
@@ -14,14 +19,16 @@
   let routingNumber = '';
   let checkingAccount = '';
   let idNumber = '';
+  let country = '';
 
   function selectTab(tab) {
     selectedTab = tab;
   }
 </script>
 
-<div class="flex items-center justify-center p-4 w-[996px] mx-auto">
-  <div class="w-[996px] mx-auto p-6">
+
+<div class="flex items-center justify-center w-full p-4 mx-auto font-['Inter']">
+  <div class="w-full sm:w-[996px] mx-auto p-6">
     <div class="mb-6">
       <h2 class="justify-start mb-8 text-2xl font-semibold text-gray-800">Payment Method</h2>
       <div class="grid grid-cols-3 gap-2">
@@ -57,104 +64,119 @@
       </div>
     </div>
 
-    {#if selectedTab === 'Card'}
-      <div class="mb-4">
-        <label class="block mb-1 text-sm text-gray-600">Card number</label>
-        <input
-          type="text"
-          class="w-full p-2 bg-gray-100 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
-          bind:value={cardNumber}
-          placeholder="XXXX XXXX XXXX XXXX"
-        />
-      </div>
-      <div class="grid grid-cols-2 gap-2 mb-4">
-        <div>
-          <label class="block mb-1 text-sm text-gray-600">Expiry</label>
-          <input
-            type="text"
-            class="w-full p-2 bg-gray-100 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
-            bind:value={expiry}
-            placeholder="MM / YY"
-          />
-        </div>
-        <div>
-          <label class="block mb-1 text-sm text-gray-600">CVC</label>
-          <input
-            type="text"
-            class="w-full p-2 bg-gray-100 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
-            bind:value={cvc}
-            placeholder="CVC"
-          />
-        </div>
-      </div>
+    <div class="w-full">
+      {#if selectedTab === 'Card'}
+        <div class="space-y-4">
+          <div>
+            <label class="block mb-1 text-sm text-gray-600">Card number</label>
+            <input
+              type="text"
+              class="w-full p-2 border-2 border-[#0b383c] rounded-full h-[47px] focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
+              bind:value={cardNumber}
+              placeholder="XXXX XXXX XXXX XXXX"
+            />
+          </div>
+          
+          <div class="grid grid-cols-1 gap-2 sm:grid-cols-2">
+            <div>
+              <label class="block mb-1 text-sm text-gray-600">Expiry</label>
+              <input
+                type="text"
+                class="w-full p-2 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
+                bind:value={expiry}
+                placeholder="MM / YY"
+              />
+            </div>
+            <div>
+              <label class="block mb-1 text-sm text-gray-600">CVC</label>
+              <input
+                type="text"
+                class="w-full p-2 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
+                bind:value={cvc}
+                placeholder="CVC"
+              />
+            </div>
+          </div>
 
-      <div class="grid grid-cols-2 gap-2 mb-4">
-        <div>
-          <label class="block mb-1 text-sm text-gray-600">Country</label>
-          <input
-            type="text"
-            class="w-full p-2 bg-gray-100 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
-            bind:value={expiry}
-            placeholder="MM / YY"
-          />
+          <div class="grid grid-cols-1 gap-2 sm:grid-cols-2">
+            <div>
+              <label class="block mb-1 text-sm text-gray-600">Country</label>
+              <select
+              id="country"
+              name="country"
+              bind:value={country}
+              class="w-full p-2 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
+              aria-label="Select project country"
+            >
+              <option value="" class="w-full">Select a country</option>
+              {#each countryList as countryOption}
+                <option value={countryOption.name}>{countryOption.name}</option>
+              {/each}
+            </select>
+            </div>
+            <div>
+              <label class="block mb-1 text-sm text-gray-600">ZIP</label>
+              <input
+                type="number"
+                class="w-full p-2 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
+                bind:value={zip}
+                placeholder="ZIP"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <label class="block mb-1 text-sm text-gray-600">ZIP</label>
-          <input
-            type="number"
-            class="w-full p-2 bg-gray-100 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
-            bind:value={zip}
-            placeholder="ZIP"
-          />
+      {:else if selectedTab === 'Crypto'}
+        <div class="w-full">
+          <Wallet />
         </div>
-      </div>
-    {:else if selectedTab === 'Crypto'}
-      <Wallet />
-    {:else if selectedTab === 'Bank'}
-      <div class="mb-4">
-        <label class="block mb-1 text-sm text-gray-600">Name</label>
-        <input
-          type="text"
-          class="w-full p-2 bg-gray-100 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
-          bind:value={name}
-          placeholder="Enter account holder's name"
-        />
-      </div>
-      <div class="mb-4">
-        <label class="block mb-1 text-sm text-gray-600">Routing Number</label>
-        <input
-          type="text"
-          class="w-full p-2 bg-gray-100 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
-          bind:value={routingNumber}
-          placeholder="Enter routing number"
-        />
-      </div>
-      <div class="mb-4">
-        <label class="block mb-1 text-sm text-gray-600">Checking Account Number</label>
-        <input
-          type="text"
-          class="w-full p-2 bg-gray-100 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
-          bind:value={checkingAccount}
-          placeholder="Enter checking account number"
-        />
-      </div>
-      <div class="mb-4">
-        <label class="block mb-1 text-sm text-gray-600">ID Number</label>
-        <input
-          type="text"
-          class="w-full p-2 bg-gray-100 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
-          bind:value={idNumber}
-          placeholder="Enter ID number"
-        />
-      </div>
+      {:else if selectedTab === 'Bank'}
+        <div class="space-y-4">
+          <div>
+            <label class="block mb-1 text-sm text-gray-600">Name</label>
+            <input
+              type="text"
+              class="w-full p-2 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
+              bind:value={name}
+              placeholder="Enter account holder's name"
+            />
+          </div>
+          <div>
+            <label class="block mb-1 text-sm text-gray-600">Routing Number</label>
+            <input
+              type="text"
+              class="w-full p-2 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
+              bind:value={routingNumber}
+              placeholder="Enter routing number"
+            />
+          </div>
+          <div>
+            <label class="block mb-1 text-sm text-gray-600">Checking Account Number</label>
+            <input
+              type="text"
+              class="w-full p-2 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
+              bind:value={checkingAccount}
+              placeholder="Enter checking account number"
+            />
+          </div>
+          <div>
+            <label class="block mb-1 text-sm text-gray-600">ID Number</label>
+            <input
+              type="text"
+              class="w-full p-2 border-2 border-[#0b383c] rounded-full focus:outline-none focus:border-[#0b383c] transition-colors duration-200"
+              bind:value={idNumber}
+              placeholder="Enter ID number"
+            />
+          </div>
 
-      <!-- Disclaimer -->
-      <p class="mb-6 text-sm text-gray-500">For banks in the US only.</p>
-    {/if}
+          <p class="mb-6 text-sm text-gray-500">For banks in the US only.</p>
+        </div>
+      {/if}
 
-    <!-- Buttons -->
-    <div class="flex justify-end">
-      <button class="px-4 py-3 text-white text-base bg-[#0b383c] rounded-full w-[20%]">Save</button>
+      <div class="flex justify-end mt-4 ">
+        <button class="px-4 py-3 text-white text-base bg-[#0b383c] rounded-full w-[20%] max-md:w-[40%]">Save</button>
+      </div>
     </div>
   </div>
 </div>
+
+
--- src/lib/Wallet.svelte
+++ src/lib/Wallet.svelte
@@ -9,27 +9,33 @@
       modalOpen.set(true);
     }
   </script>
-  
-  <main class="flex overflow-hidden flex-col items-center px-20 pt-14 leading-none text-center bg-white pb-[20px] rounded-[100px] max-md:px-5 max-md:py-24">
-    <section class="flex flex-col items-center w-full max-w-[1034px] max-md:max-w-full">
-     
-      <div class="flex flex-col self-stretch w-full mt-20 max-md:mt-10 max-md:max-w-full">
-        <button on:click={openWalletPopup} class="gap-1.5 self-center px-6 py-4 w-[80%] text-3xl font-semibold text-lime-100 bg-lime-800 rounded-[53px] max-md:px-5 max-md:max-w-full">
+
+  <main class="flex overflow-hidden flex-col items-center px-4 sm:px-6 pt-14 leading-none text-center bg-white pb-[20px] max-md:mt-[-80px]">
+    <section class="flex flex-col items-center w-full max-w-full sm:max-w-[1034px]">
+      <div class="flex flex-col self-stretch w-full mt-20 ">
+        <button
+          on:click={openWalletPopup}
+          class="gap-1.5 self-center px-6 py-4 w-[80%] text-3xl font-semibold text-lime-100 bg-lime-800 rounded-[53px] sm:px-5 sm:w-full max-md:text-xl "
+        >
           Connect Wallet
         </button>
-        <div class="flex flex-col self-center mt-24 max-w-full text-black w-[996px] max-md:mt-10">
-          <label for="amount" class="max-w-full text-4xl flex justify-start font-semibold w-[387px]">
+  
+        <div class="flex flex-col self-center w-full max-w-full mt-24 sm:mt-10">
+          <label for="amount" class="flex justify-start text-4xl font-semibold sm:text-xl sm:w-full max-md:text-3xl">
             Enter Amount
           </label>
           <input
             type="text"
             id="amount"
-            class="gap-1.5 self-stretch py-3 pr-16 pl-7 mt-8 w-full text-3xl font-thin border-lime-800 border-solid border-[3px] min-h-[67px] rounded-[50px] max-md:px-5"
+            class="gap-1.5 self-stretch py-3 pr-16 pl-7 mt-8 w-full text-3xl font-thin border-lime-800 border-solid border-[3px] h-[67px] rounded-[50px] sm:px-5 sm:text-lg sm:max-w-full max-md:h-[50px]"
             placeholder="$ USDC"
           />
         </div>
-
-        <button on:click={openWalletPopup} class="hidden gap-1.5 self-center px-2 py-4 w-[20%] text-xl font-semibold mt-6 text-white bg-lime-800 rounded-[53px] max-md:px-5 max-md:max-w-full">
+  
+        <button
+          on:click={openWalletPopup}
+          class="hidden gap-1.5 self-center px-2 py-4 w-[20%] text-xl font-semibold mt-6 text-white bg-lime-800 rounded-[53px] sm:px-5 sm:w-full sm:max-w-full sm:hidden"
+        >
           Send
         </button>
       </div>
@@ -43,3 +49,4 @@
       </div>
     </div>
   {/if}
+  
