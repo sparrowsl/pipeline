2fd08cba506614f40a1d9c207d094a9ba269236f
sparrowsl <benthorpe.dev@gmail.com>
2024-12-11 10:59:12 +0000

    feat: add tailwindcss forms library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    "./src/**/*.svelte",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js,svelte,ts}", "./src/**/*.svelte"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/forms")],
+};
+
