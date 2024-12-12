ee7e845480f721c9f9f74139d4b668f5fef26efe
sparrowsl <benthorpe.dev@gmail.com>
2024-12-12 14:57:55 +0000

    fix: broken tailwindcss plugin config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    "./src/**/*.svelte",
-  ],
+  content: ['./src/**/*.{html,js,svelte,ts}', './src/**/*.svelte'],
   theme: {
     extend: {},
   },
-  plugins: [
-	require("@tailwindcss/forms")
-  ],
-}
-
+  plugins: [import('@tailwindcss/forms')],
+};
