9a0995dd4bcdd18572f73c1879c3cada7c58f09f
Hawa Kallon <97176661+HawaKallon@users.noreply.github.com>
2025-02-26 11:50:07 +0000

    update git update design #279 (#280)

--- src/lib/GitUpdate.svelte
+++ src/lib/GitUpdate.svelte
@@ -1,30 +1,91 @@
 <script>
     export let update;
+    import Icon from '@iconify/svelte';
+    import { dateTimeFormat } from '$lib/utils/dateTimeFormat.js';
+
+  let date = '';
+  date = dateTimeFormat(update.created_at);
+
 </script>
 
 
 
-<div class="relative flex items-start gap-3 text-[#0B383C] border border-gray-200 shadow-lg p-6 bg-white rounded-2xl w-full">
-  <img src={update.user.avatar_url} alt="Avatar" class="border-2 border-[#0B383C] rounded-full shadow-md w-16 h-16" />
+ <div class="inline-flex flex-col items-start justify-start w-full p-4 bg-white md:p-9">
+    <div class="flex flex-col items-start self-stretch justify-start gap-2">
+      <div class="flex w-full flex-col items-start justify-start gap-[13.30px] pt-[1.75px]">
+        <div
+          class="self-stretch font-['Inter'] text-2xl font-bold leading-tight text-[#282828] md:text-[32px] md:leading-10"
+        >
+          {update.user.userProfile}
+        </div>
+        <div
+          class="flex flex-col items-start justify-start gap-1 self-stretch border-b border-[#dcdedd] pb-5"
+        >
+          <div class="inline-flex items-center justify-start w-full gap-3">
+            <img
+              loading="lazy"
+              src={update.user.avatar_url}
+              alt="User Profile"
+              class="h-10 w-10 rounded-full border border-[#dcdedd] md:h-[42px] md:w-[42px]"
+            />
+  
+            <div class="inline-flex flex-col justify-between w-full">
+              <div class="flex justify-between">
+             
+                <div class="inline-flex items-center self-stretch gap-2">
+                <div
+                  class="max-w-[200px] truncate font-['Inter'] text-sm font-normal leading-normal text-[#282828] md:max-w-none"
+                >
+                <a href={update.user.html_url} target="_blank" class="text-2xl font-bold text-[#0B383C] hover:underline">
+                    {update.user.login}
+                  </a>
+                </div>
+                <div
+                  class="inline-flex flex-col items-start justify-center rounded-[3px] bg-[#05ce78] px-[5px] py-[0.25px]"
+                >
+                  <div class="font-['Inter'] text-xs font-bold leading-[18px] text-white">
+                    <!-- {update.user_id === update.userProfile.user_id ? 'Creator' : 'Member'} -->
+                    {update.user.type}
+                  </div>
+                </div>
+            </div>
 
-  <div class="flex flex-col w-full ">
+                <div class="flex justify-end mt-2">
+                    <a href={update.merged_url} target="_blank" class="px-1 py-1 font-mediu text-[0.625rem] text-white bg-[#0B383C] rounded-md hover:bg-opacity-80 transition">
+                      View PR Code
+                    </a>
+                  </div>
 
-    <div class="flex justify-between">
-        <a href={update.user.html_url} target="_blank" class="text-2xl font-bold text-[#0B383C] hover:underline">
-          {update.user.login}
-        </a>
-      
-        <div class="flex justify-end mt-2">
-          <a href={update.merged_url} target="_blank" class="px-1 py-1 font-mediu text-[0.625rem] text-white bg-[#0B383C] rounded-md hover:bg-opacity-80 transition">
-            View PR Code
-          </a>
+                
+                
+              </div>
+
+              
+              <div
+                class="self-stretch font-['Inter'] text-[13px] font-normal leading-[18px] text-[#282828]/50"
+              >
+              {date}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      
-    <p class="w-full mt-1 text-sm text-gray-700">
-      <!-- {description} -->
-    </p>
-
-
+      <div class="flex flex-col items-start self-stretch justify-start gap-1">
+        <div class="flex flex-col items-start justify-start gap-[30px] self-stretch pb-2">
+          <div
+            class="self-stretch font-['Inter'] text-base font-normal leading-relaxed text-[#282828]"
+          >
+           <!-- {description} -->Project description
+          </div>
+        </div>
+      </div>
+      <div class="inline-flex items-center justify-between w-full">
+        <div class="flex items-center justify-start gap-6">
+          <div class="flex items-center justify-start gap-2">
+            <Icon icon="radix-icons:commit" class="text-2xl text-[#8C8C8C]" />
+            <div class="font-['Inter'] text-sm font-normal leading-normal text-[#9b9e9e]">16</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
