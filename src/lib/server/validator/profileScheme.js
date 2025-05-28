99a87b92f5bf312526b609c218089aa8d01b9f02
Colin Ogoo <ogoo.colin@gmail.com>
2025-05-28 12:24:16 +0000

    fix(profile validation): remove requirement for country from schema

--- src/lib/server/validator/profileScheme.js
+++ src/lib/server/validator/profileScheme.js
@@ -5,7 +5,7 @@ export const profileSchema = z.object({
   image: z.string().url('Invalid image URL').optional(),
   banner: z.string().url('Invalid banner image URL').optional(),
   bio: z.string().min(1),
-  country: z.string().min(1),
+  // country: z.string().min(1),
   // github: z.string().url('Invalid GitHub URL').optional(),
   // discord: z.string().url('Invalid LinkedIn URL').optional(),
   // twitter: z.string().url('Invalid Twitter URL').optional(),
